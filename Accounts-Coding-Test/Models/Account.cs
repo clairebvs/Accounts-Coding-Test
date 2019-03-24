e51cccc7c0056bc97ef940ec6a5f0a713853df99
ClaireBvs <clairembeauvais@gmail.com>
2019-03-24 10:27:36 -0600

    Remove extra class AccountExtensions from Account.cs

--- Accounts-Coding-Test/Models/Account.cs
+++ Accounts-Coding-Test/Models/Account.cs
@@ -3,17 +3,9 @@
 namespace AccountsCodingTest.Models
 {
     public class Account
-    {         public Object getUserAccount()         {             string url = "https://frontiercodingtests.azurewebsites.net/api/accounts/getall";             var client = new WebClient();             var content = client.DownloadString(url);
+    {
+        public Object getUserAccount()         {             string url = "https://frontiercodingtests.azurewebsites.net/api/accounts/getall";             var client = new WebClient();             var content = client.DownloadString(url);
 
             object jsonData = JsonConvert.DeserializeObject(content);             return jsonData;         }
-
-
-        // Extension method used to add the middleware to the HTTP request pipeline.
-        public static class AccountExtensions
-    {
-        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<Account>();
-        }
     }
 }
